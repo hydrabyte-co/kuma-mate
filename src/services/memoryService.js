8bc8b175e5d8e3be49cea386320e3f112d29a9a0
Tony Hoang <tonyh@hydrabyte.co>
2025-04-13 02:24:32 +0700

    Fix memory usage calculations in memoryService.js to use available memory instead of used memory

--- src/services/memoryService.js
+++ src/services/memoryService.js
@@ -1,12 +1,12 @@
 const si = require('systeminformation');
-const logger = require('../utils/logger');
+const logger = require('../../utils/logger');
 
 const getMemoryUsage = async () => {
   try {
     const mem = await si.mem();
     return {
-      usedPercent: (mem.used / mem.total) * 100,
-      usedGB: (mem.used / 1024 / 1024 / 1024).toFixed(2),
+      usedPercent: ((mem.total - mem.available) / mem.total) * 100, // Dùng available thay vì used
+      usedGB: ((mem.total - mem.available) / 1024 / 1024 / 1024).toFixed(2),
       totalGB: (mem.total / 1024 / 1024 / 1024).toFixed(2)
     };
   } catch (err) {
